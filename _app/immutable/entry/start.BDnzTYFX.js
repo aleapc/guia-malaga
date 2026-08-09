5c538553bfd2d92a8e656272259fcd3d8c96dd0b
aleapc <47722878+aleapc@users.noreply.github.com>
2026-08-09 18:22:31 -0300

    deploy: 2026-08-09 18:22

--- _app/immutable/entry/start.BDnzTYFX.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"../chunks/DOMetDJb.js";import{m as t}from"../chunks/B7GqG8g_.js";export{t as load_css,r as start};
---
+++ _app/immutable/entry/start.BnxmbsP2.js
@@ -0,0 +1,1 @@
+import{s as r}from"../chunks/BfzQTIWa.js";import{m as t}from"../chunks/mqEXyTMG.js";export{t as load_css,r as start};
