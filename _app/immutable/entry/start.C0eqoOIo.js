6499aa7d22247845742794883ffd395887b4b072
aleapc <47722878+aleapc@users.noreply.github.com>
2026-08-10 08:56:51 -0300

    deploy: 2026-08-10 08:56

--- _app/immutable/entry/start.C0eqoOIo.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"../chunks/BCOf-hu0.js";import{m as t}from"../chunks/NerQwl8d.js";export{t as load_css,r as start};
---
+++ _app/immutable/entry/start.DJ_YHm3X.js
@@ -0,0 +1,1 @@
+import{s as r}from"../chunks/8wGqcwkr.js";import{m as t}from"../chunks/xxIDgwBh.js";export{t as load_css,r as start};
